7bc6cf7502f06fc170bcbe0ee1f66126748e7e58
hjjansen1970 <mailheinjansen@gmail.com>
2015-04-22 10:21:01 +0200

    Changes made by Hein Jansen

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,53 @@
-## [Put comments here that describe what your functions do]
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#-------------------------------------------------------------------------------------------
+# This function creates a special "matrix" object that can cache its inverse.
+#-------------------------------------------------------------------------------------------
+makeCacheMatrix <- function(x=matrix()) {
+        m <- NULL
+        
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        get <- function() x
+        
+        setinverse <- function(inverse) m <<- inverse
+        
+        getinverse <- function() m
+        
+        # Function returns a list
+        list(set=set , get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
+#-------------------------------------------------------------------------------------------
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix. 
+# If the inverse has already been calculated (and the matrix has not changed), 
+# then the cachesolve should retrieve the inverse from the cache.
+#-------------------------------------------------------------------------------------------
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function (x,...) {
+        m <- x$getinverse()
+        
+        # If m is not null show cached version of inverse matrix
+        if (!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        
+        data <- x$get() #get matrix
+        
+        # Try to create inverse of matrix
+        result <- tryCatch({
+                m <- solve(data, ...) #Create inverse of matrix
+        }, warning=function(war) {
+                print(paste("Warning: ",war))
+        }, error=function(err) {
+                        print(paste("Error occured: ",err))
+                }
+        )
+        
+        x$setinverse(m)
+        
+        #Function returns inverse of matrix
+        m
 }
